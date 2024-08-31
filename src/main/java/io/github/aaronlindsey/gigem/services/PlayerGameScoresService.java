a725395feead37d502ebea84c6b6d090029aa51e
Aaron Lindsey <1781978+aaronlindsey@users.noreply.github.com>
2024-08-30 20:44:49 -0700

    Normalize scores to the best score for each game
    Currently, scores are calculated as the distance between the actual game
    score and the player's prediction. The best score per game is the one
    closest to the actual game score (i.e., the one with the lowest
    distance).
    
    This change takes the best score for each game and normalizes all the
    other scores to it. In other words, now the scores are calculated as teh
    distance between the best score and the player's prediciton.

--- src/main/java/io/github/aaronlindsey/gigem/services/PlayerGameScoresService.java
+++ src/main/java/io/github/aaronlindsey/gigem/services/PlayerGameScoresService.java
@@ -10,15 +10,19 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.UUID;
 
 import static io.github.aaronlindsey.gigem.utilities.Utilities.toStream;
 import static java.lang.Math.abs;
+import static java.util.Collections.min;
 import static java.util.stream.Collectors.toMap;
 
 @Service
 public class PlayerGameScoresService {
+  private static final int DEFAULT_PREDICTION = 0;
+
   private final PlayerRepository playerRepository;
   private final GameRepository gameRepository;
   private final PlayerGamePredictionsService playerGamePredictionsService;
@@ -57,26 +61,31 @@ public class PlayerGameScoresService {
       return Optional.empty();
     }
 
-    Map<UUID, Integer> scoresByPlayer = toStream(playerRepository.findAll())
-        .map(Player::getId)
-        .collect(toMap(id -> id, id -> calculateScoreForPlayerAndGame(id, game, predictionsByPlayer.get())));
-
-    return Optional.of(scoresByPlayer);
+    return Optional.of(calculateScoresByPlayer(game.getScore(), predictionsByPlayer.get()));
   }
 
-  private int calculateScoreForPlayerAndGame(UUID playerId, Game game, Map<UUID, Integer> predictionsByPlayer) {
-    if (!predictionsByPlayer.containsKey(playerId)) {
-      // If the player did not submit a prediction, their score is equal to the game score.
-      return game.getScore();
+  private Map<UUID, Integer> calculateScoresByPlayer(int gameScore, Map<UUID, Integer> predictionsByPlayer) {
+    // Fill in missing predictions with the default value.
+    toStream(playerRepository.findAll())
+      .map(Player::getId)
+      .forEach(id -> predictionsByPlayer.putIfAbsent(id, DEFAULT_PREDICTION));
+
+    // Each players' score starts as the distance between the game score and their prediction.
+    Map<UUID, Integer> denormalizedScores = predictionsByPlayer.entrySet().stream()
+        .collect(toMap(Entry::getKey, e -> abs(gameScore - e.getValue())));
+
+    // Then, the players' scores are normalized to the best score.
+    int bestScore = min(denormalizedScores.values());
+    Map<UUID, Integer> normalizedScores = denormalizedScores.entrySet().stream()
+        .collect(toMap(Entry::getKey, e -> e.getValue() - bestScore));
+
+    // If a player predicts the exact game score they get a bonus.
+    if (bestScore == 0) {
+      Map<UUID, Integer> scoresWithBonus = normalizedScores.entrySet().stream()
+          .collect(toMap(Entry::getKey, e -> e.getValue() == 0 ? exactPredictionBonus : e.getValue()));
+      return scoresWithBonus;
     }
 
-    int playerPrediction = predictionsByPlayer.get(playerId);
-    if (game.getScore().equals(playerPrediction)) {
-      // If the player predicts the exact game score they get a bonus.
-      return exactPredictionBonus;
-    }
-
-    // The player's score is the distance between the game score and their prediction.
-    return abs(game.getScore() - playerPrediction);
+    return normalizedScores;
   }
 }
